8d85aae298c1c7cdb661056e9317b3af6535d7f6
Admin <admin@mac-admin.local>
2015-12-03 20:17:50 +1000

    Поехали!

---
+++ helper/helper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helper
+//
+//  Created by Admin on 03.12.15.
+//  Copyright © 2015 PNU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
